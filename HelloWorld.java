e892bffd4c44e81c0dd4eb2e2ce3f09a7bc3d3ff
onkarcoder750 <onkark750@gmail.com>
2024-10-26 19:26:09 +0530

    Add line2 and line 3 in HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Line1 from HelloWorld..");
+    System.out.println("Line2 from HelloWorld..");
+    System.out.println("Line3 from HelloWorld..");
   }
 }
